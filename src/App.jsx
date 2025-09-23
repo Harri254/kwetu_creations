bae8202fd38c34c05f930156015b55003ded6d17
harri254 <harrison2002sirya@gmail.com>
2025-09-23 08:15:00 +0300

    many changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import './index.css'
 import Header from './layout/Header'
@@ -9,21 +9,23 @@ import Contact from './pages/Contact'
 import Footer from './layout/Footer'
 import "./icons.js";
 
+
 function App() {
+
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/product' element={<Product/>}></Route>
         <Route path='/product/:category' element={<Product/>}></Route>
+        {/* <Route path='/services' element={<Body/>}></Route> */}
         <Route path='/about' element={<About/>}></Route>
         <Route path='/contact' element={<Contact/>}></Route>
       </Routes>
       <Footer/>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
 export default App
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-// import { HashRouter } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <StrictMode>
     <App />
-  </HashRouter>,
+  </StrictMode>,
 )
