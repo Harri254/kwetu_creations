960a6e7b0dcd1d4bebff6428bbebe3001618b373
harri254 <harrison2002sirya@gmail.com>
2026-04-02 16:02:12 +0300

    Add backend

--- src/icons.js
+++
@@ -1,6 +0,0 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons"; 
-import { fab } from "@fortawesome/free-brands-svg-icons"; 
-
-// Add whichever icon sets you need
-library.add(fas, fab);
